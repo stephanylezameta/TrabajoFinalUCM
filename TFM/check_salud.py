59ffd9760c122f562ef54a8d879073f822fd4557
quirkplay <jarodriguezr95@gmail.com>
2026-08-12 21:04:13 +0200

    Agrega script de chequeo de salud/integridad de la base de datos

---
+++ TFM/check_salud.py
@@ -0,0 +1,9 @@
+﻿import sqlite3
+conn = sqlite3.connect('data/tui_recomendador.db')
+cur = conn.cursor()
+cur.execute('PRAGMA integrity_check;')
+print('Integridad:', cur.fetchone())
+cur.execute('SELECT COUNT(*) FROM resenas')
+print('Total resenas:', cur.fetchone()[0])
+cur.execute("SELECT COUNT(*) FROM resenas WHERE texto_original IS NULL OR LENGTH(texto_original) < 15")
+print('Resenas vacias/muy cortas:', cur.fetchone()[0])
